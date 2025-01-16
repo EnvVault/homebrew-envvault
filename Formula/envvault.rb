fcb8ac15e4fbaf07adf946b6d596d04841cb0e32
muhammed sanjith <46193060+sanjithmsk@users.noreply.github.com>
2025-01-16 19:55:08 +0530

    Update envvault.rb
    docur changes added

--- Formula/envvault.rb
+++ Formula/envvault.rb
@@ -1,8 +1,8 @@
 class Envvault < Formula
     desc "Secure, scalable, and collaborative environment variable management"
     homepage "https://envvault.com/"
-    url "https://envvault.com/releases/envvault-0.0.4.tar.gz"
-    sha256 "eca25074c3d03da142c9646b2007f255738bb9ed7e9a4af86035ccd77f9aa53a"
+    url "https://envvault.com/releases/envvault-0.0.5.tar.gz"
+    sha256 "c67f71516970bed72b807bb9c49e07e3bc1b86ecc7a75552ba922e8355d552b1"
     license "MIT"
   
     depends_on "libtool" => :build
