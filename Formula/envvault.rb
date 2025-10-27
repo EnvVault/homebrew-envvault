0ff284d5b74c527da9dc19d0a4fe9906ef360f24
muhammed sanjith <46193060+sanjithmsk@users.noreply.github.com>
2025-10-27 21:29:35 +0530

    Update envvault.rb

--- Formula/envvault.rb
+++ Formula/envvault.rb
@@ -1,8 +1,8 @@
 class Envvault < Formula
     desc "Secure, scalable, and collaborative environment variable management"
     homepage "https://envvault.com/"
-    url "https://envvault.com/releases/envv-1.0.1.tar.gz"
-    sha256 "44f9c2dbd455a858a95b1779b61643db59e496a71baa95de3a68cfc0ebeac820"
+    url "https://envvault.com/releases/envv-1.0.2.tar.gz"
+    sha256 "1d7f0caab0ce931aac0c5705674eb981d5c984e6067c57e721397360f6f93617"
     license "MIT"
   
     depends_on "libtool" => :build
